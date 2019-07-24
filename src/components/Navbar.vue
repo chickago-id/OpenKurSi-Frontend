d843c3d8c2dc821f123269af30288afba98711a7
Adi Priyanto <adipriyanto023@gmail.com>
2019-07-24 13:17:50 +0700

    add login form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,12 +16,66 @@
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
         <b-navbar-nav>
-          <b-nav-item href="#">Login</b-nav-item>
+          <div>
+            <b-nav-item id="show-btn" @click="showModal" href="#">Login</b-nav-item>
+            <b-modal ref="my-modal" hide-footer title="Login">
+              <div class="d-block ">
+                  <form ref="form" @submit.stop.prevent="handleSubmit">
+                    <b-form-group
+                      :state="nameState"
+                      label="Username"
+                      label-for="name-input"
+                      invalid-feedback="Name is required"
+                    >
+                      <b-form-input
+                        id="username-input"
+                        v-model="name"
+                        :state="nameState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                  </form>
+                  <form ref="form" @submit.stop.prevent="handleSubmit">
+                    <b-form-group
+                      :state="nameState"
+                      label="Password"
+                      label-for="name-input"
+                      invalid-feedback="Name is required"
+                    >
+                      <b-form-input
+                        id="pass-input"
+                        type ="password"
+                        v-model="name"
+                        :state="nameState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                  </form>
+              </div>
+            <b-button class="mt-3" block @click="hideModal">Login</b-button>
+            </b-modal>
+          </div>
       </b-navbar-nav>
 
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </div>
-
 </template>
+<script>
+  export default {
+    methods: {
+      showModal() {
+        this.$refs['my-modal'].show()
+      },
+      hideModal() {
+        this.$refs['my-modal'].hide()
+      },
+      toggleModal() {
+        // We pass the ID of the button that we want to return focus to
+        // when the modal has hidden
+        this.$refs['my-modal'].toggle('#toggle-btn')
+      }
+    }
+  }
+</script>
