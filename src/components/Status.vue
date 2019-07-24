b990fc2671f130952b15a6a78b8ea9b4d182ef33
Ubed <ubaidillahmh@gmail.com>
2019-07-24 16:40:17 +0700

    add afterlogin

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,25 +1,29 @@
 <template>
-    <div class="container">
-        <div class="card">
-            <div class="card-header">
-                <h3>Status Kursus</h3>
-            </div>
-            <div class="card-body">
-                <table class="table-responsive">
-                    <tr>
-                        <th>#</th>
-                        <th>Kursus</th>
-                        <th>Jadwal</th>
-                        <th>Status</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Office</td>
-                        <td>20 Agustus 2019</td>
-                        <td>Aktif</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
+    <div>
+        <b-container>
+            <b-card
+                title="Status"
+            >
+                <b-table striped hover :items="items" :fields="fields">
+                    <template slot="Action" slot-scope="row">
+                        <b-button size="sm" variant="warning">Edit</b-button>&nbsp
+                        <b-button size="sm" variant="danger">Hapus</b-button>
+                    </template>
+                </b-table>
+            </b-card>
+        </b-container>
     </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: ['No', 'Kelas', 'Jadwal', 'Status', 'Action'],
+        items: [
+          { No: 1, Kelas: 'Office', Jadwal: '11-Agustus-2019', Status: 'Aktif' },
+        ]
+      }
+    }
+  }
+</script>
--- src/views/Afterlogin.vue
+++ src/views/Afterlogin.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-        <status/>
+        <status/><br>
+        <addkelas/>
     </div>
 </template>
 
 <script>
 import status from '@/components/Status.vue';
+import addkelas from '@/components/Addkelas.vue';
 
 export default {
     components:{
-        status
+        status,
+        addkelas
     }
 }
 </script>
