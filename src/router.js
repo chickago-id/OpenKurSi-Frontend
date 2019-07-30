4db45180cf5c2b34a8bbf4922a70391a2e0e1ca0
Ubed <ubaidillahmh@gmail.com>
2019-07-30 11:14:23 +0700

    update routerjs

--- src/router.js
+++ src/router.js
@@ -4,7 +4,58 @@ import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
-export default new Router({
+// export default new Router({
+//   routes: [
+//     {
+//       path: "/",
+//       name: "home",
+//       component: Home
+//     },
+//     {
+//       path: "/afterlogin",
+//       name: "afterlogin",
+//       component: () => import("./views/Afterlogin.vue")
+//     },
+//     {
+//       path: '/admin/kelas',
+//       name: 'kelas',
+//       component: () => import('./views/Vkelas.vue')
+//     },
+//     {
+//       path: '/admin/kelas/tambah',
+//       name: 'tambah_kelas',
+//       component: () => import('./views/Vnewkelas.vue')
+//     },
+//     {
+//       path: "/jadwal",
+//       name: "jadwal",
+//       component: () => import("./views/Penjadwal.vue")
+//     },
+//     {
+//       path: "/materi",
+//       name: "materi",
+//       component: () => import("./views/Penmateri.vue")
+//     },
+//     {
+//       path: "/nilai",
+//       name: "nilai",
+//       component: () => import("./views/Pennilai.vue")
+//     },
+//     {
+//       path: "/absensi",
+//       name: "absensi",
+//       component: () => import("./views/Absensi.vue") 
+//     },
+//     {
+//       path: "/fill",
+//       name: 'lengkapi',
+//       component: () => import('./views/Vlengkapi.vue')
+//     }
+//   ]
+// });
+
+let router = new Router({
+  mode: 'history',
   routes: [
     {
       path: "/",
@@ -52,4 +103,18 @@ export default new Router({
       component: () => import('./views/Vlengkapi.vue')
     }
   ]
-});
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/') 
+  } else {
+    next() 
+  }
+})
+
+export default router
