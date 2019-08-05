e83e790e45b6e62413ce478e7dc2fe7af8cb86f6
Adi Priyanto <adipriyanto023@gmail.com>
2019-08-05 14:11:24 +0700

    add

--- src/components/Materi.vue
+++ src/components/Materi.vue
@@ -1,30 +1,41 @@
 <template>
   <div>
+
     <b-container class="tengah">
       <b-card>
         <div class="materitengah">
           <h2>Materi</h2>
           <div class="text-right">
-            <loginnav />
+              <b-button
+              v-b-modal.modal-sm
+              variant="primary"
+              id="show-btn"
+              @click="showModal"
+              href="#"
+            >Tambah</b-button>
+              <b-modal
+              variant="primary"
+              title="Materi"
+              header-bg-variant="warning"
+              size="sm"
+              ref="my-modal"
+              hide-footer
+              style="text-center"
+              >
+            <form @submit.prevent="setMateri" >
+              <label>Nama Materi</label>
+              <b-form-input v-model="data_materi.nama_materi" class="col-8"></b-form-input>
+              <label for="input-with-list">Kode Materi</label>
+              <b-form-input v-model="data_materi.kode_materi" class="col-5"></b-form-input>
+              <div style="text-align:center;">
+                <b-button variant="primary" class="mt-3 btn-sm asd" type="submit" block>Tambah Materi</b-button>
+              </div>
+            </form> 
+            </b-modal>
           </div>
           <br />
         </div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Kode Materi</th>
-              <th>Materi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td >1</td>
-              <td >12sum13</td>
-              <td >sumof</td>
-            </tr>
-          </tbody>
-        </table>
+            <b-table striped hover :items="materi" :fields="fields"> </b-table>  
       </b-card>
     </b-container>
   </div>
@@ -32,32 +43,69 @@
 
 
 <script>
-import loginnav from "./Popmateri.vue";
 import axios from "axios";
 export default {
   components: {
-    loginnav
+   
   },
   data(){
     return{
-      data_materi:{
-        no:'',
-        kode_materi:'',
-        materi:''
+      fields:{
+          id:{
+            label:'No',
+            sortable : true
+          },
+          kode_materi:{
+            label:'Kode Materi',
+            sortable : true
+          },
+          nama_materi:{
+            label:'Nama Materi',
+            sortable : true
+          },
+          action:{
+
+          }
       },
+      data_materi:{
+          nama_materi :'',
+          kode_materi :''
+        },
+
       materi:[]
+
     }
   },
   mounted(){
     this.getMateri()
   },
+  
   methods:{
     getMateri(){
-      axios.get("")
+      axios.get(process.env.VUE_APP_ROOT_API+'/materi')
       .then((response) => {
-        this.materi = response.data.materi;
+        this.materi = response.data.data;
         console.log(response)
       })
+    },
+    showModal() {
+      this.$refs["my-modal"].show();
+    },
+    hideModal() {
+      this.$refs["my-modal"].hide();
+    },
+    toggleModal() {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs["my-modal"].toggle("#toggle-btn");
+    },
+    setMateri(event){
+      axios.post(process.env.VUE_APP_ROOT_API+'/materi' ,this.data_materi)
+      // this.materi = res.data
+      // this.data_materi.kode_materi = ''
+      // this.data_materi.nama_materi =''
+      event.target.reset();
+      this.$refs["my-modal"].hide();
     },
    
     editMateri(){
