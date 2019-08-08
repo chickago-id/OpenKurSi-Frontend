108e6346635b790743b55e83e942dbc2e739aa71
kaijack <jakkse7en@gmail.com>
2019-08-08 03:12:15 -0400

    post jadwal

--- src/views/Jadwal.vue
+++ src/views/Jadwal.vue
@@ -6,27 +6,6 @@
       <div class="text-center">
         <b-table striped hover :items="jadwal"></b-table>
       </div>
-      <!-- <div>
-        <div class="post" v-for="user in users" :key="user.id">
-          <div>
-            <table class="table text-center">
-              <tr>
-                <td>nama kelas</td>
-                <td>kode kelas</td>
-                <td>tanggal</td>
-                <td>jam pilihan</td>
-              </tr>
-              <tr>
-                <th>{{ user.nama_kelas }}</th>
-                <th>{{ user.id_kelas }}</th>
-                <th>{{ user.tanggal }}</th>
-                <th>{{ user.jam_pilihan }}</th>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </div>-->
-      <div></div>
     </b-card>
   </b-container>
 </template>
