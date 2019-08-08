45b048f35bbef25da78c23e72b846765be40f1f6
kaijack <jakkse7en@gmail.com>
2019-08-08 03:10:14 -0400

    post jadwal

--- src/views/Jadwal.vue
+++ src/views/Jadwal.vue
@@ -1,21 +1,56 @@
 <template>
-	<b-container>
-		<b-card>
-			<div>
-    			<b-table striped hover :items="jadwal"></b-table>
- 			 </div>
-		</b-card>
-	</b-container>
+  <b-container>
+    <b-card>
+      <h1>Jadwal</h1>
+      <br />
+      <div class="text-center">
+        <b-table striped hover :items="jadwal"></b-table>
+      </div>
+      <!-- <div>
+        <div class="post" v-for="user in users" :key="user.id">
+          <div>
+            <table class="table text-center">
+              <tr>
+                <td>nama kelas</td>
+                <td>kode kelas</td>
+                <td>tanggal</td>
+                <td>jam pilihan</td>
+              </tr>
+              <tr>
+                <th>{{ user.nama_kelas }}</th>
+                <th>{{ user.id_kelas }}</th>
+                <th>{{ user.tanggal }}</th>
+                <th>{{ user.jam_pilihan }}</th>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>-->
+      <div></div>
+    </b-card>
+  </b-container>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        jadwal: [
-          { nama_kelas:'Office',id_kelas:'Off1',tanggal:'15-10-2019',jam_pilihan:'15.00' },
-        ]
-      }
-    },
-    }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      jadwal: []
+    };
+  },
+  mounted() {
+    var self = this;
+    axios
+      .get("https://api.myjson.com/bins/19xoct")
+      .then(function(res) {
+        self.jadwal = res.data;
+        console.log("Data: ", res.data);
+      })
+      .catch(function(error) {
+        console.log("Error: ", error);
+      });
+  }
+};
 </script>
+
