268a5fef34ac5bf6ee8ff890bdbb942591f09bdd
akbarlaz <akbar.lazuardi@yahoo.com>
2019-09-23 14:06:54 +0700

    add view kategori nilai(kurang validasi)

---
+++ src/views/KategoriNilai.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <kategoriNilai></kategoriNilai>
+  </div>
+</template>
+
+<script>
+import kategoriNilai from "@/components/KategoriNilai.vue";
+
+export default {
+  components: {
+    kategoriNilai
+  }
+};
+</script>
